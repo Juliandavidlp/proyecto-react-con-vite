85075d2f22a4b0344f2083cfa47d4abf614e9d20
Julián <juliandorao@gmail.com>
2025-04-09 20:23:37 -0300

    Julián Dorao ~ Talento Tech

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import ComponenteH1 from './components/ComponenteH1.jsx'
 import ListaUsuarios from './components/ListaUsuarios.jsx'
+import Usuario from './components/Usuario.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // Defino un objeto en JS
+  const usuario={nombre:"Julián", edad:43};
+  
   return (
     <ComponenteH1 nombre="Julián"/>
     // <ListaUsuarios/>
+    <Usuario info={usuario}/>
   )
 }
 
---
+++ src/components/Usuario.jsx
@@ -0,0 +1,9 @@
+function Usuario(props) {
+    return (
+        <div>
+            <p>Nombre: {props.info.nombre}</p>
+            <p>Edad: {props.info.nombre}</p>
+        </div>
+    )
+}
+export default Usuario;
